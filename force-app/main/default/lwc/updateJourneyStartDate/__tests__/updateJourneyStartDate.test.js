e3910c41284dc630ccecb0fbb52c97858f28de50
luke bowman <you@email.com>
2024-01-17 10:47:37 +0000

    Final changes, need to finish styling and comments

--- force-app/main/default/lwc/updateJourneyStartDate/__tests__/updateJourneyStartDate.test.js
+++ force-app/main/default/lwc/updateJourneyStartDate/__tests__/updateJourneyStartDate.test.js
@@ -1,4 +1,4 @@
-import { createElement } from 'lwc';
+//import { createElement } from 'lwc';
 import UpdateJourneyStartDate from 'c/updateJourneyStartDate';
 
 describe('c-update-journey-start-date', () => {
--- force-app/main/default/lwc/updateJourneyStartDate/updateJourneyStartDate.js
+++ force-app/main/default/lwc/updateJourneyStartDate/updateJourneyStartDate.js
@@ -1,31 +1,5 @@
-import { LightningElement, api, wire } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import updateTasks from '@salesforce/apex/mdUpdateJourneyStartDate.updateTasks';
+import { LightningElement } from 'lwc';
 
-export default class updateJourneyStartDate extends LightningElement {
-    @api recordId; // This will be set to the Journey Id when the Quick Action is invoked
-
-    connectedCallback() {
-        updateTasks({ journeyId: this.recordId })
-            .then(() => {
-                // Show success message
-                this.dispatchEvent(new
-                    ShowToastEvent({
-                    title: 'Success',
-                    message: 'Task dates updated successfully',
-                    variant: 'success'
-                    }));
-                    // Close the Quick Action Modal
-                    this.dispatchEvent(new CustomEvent('closequickaction'));
-                    })
-                    .catch(error => {
-                    // Show error message
-                    this.dispatchEvent(new ShowToastEvent({
-                    title: 'Error',
-                    message: error.body.message,
-                    variant: 'error'
-                    }));
-                    });
-                    }
-                    }
-
+export default class RefreshReminder extends LightningElement {
+    // No JavaScript needed for simple reminder text
+}
